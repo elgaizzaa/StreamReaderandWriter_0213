a12e72ab65fd5b71db4c54b8384df4fb495bcde7
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-06-04 15:11:08 +0700

    menunjukan ke sebuah file didalam mode menulis

--- ioFileWithPatch.cpp
+++ ioFileWithPatch.cpp
@@ -13,4 +13,27 @@ int main() {
     // Membuka file dalam mode menulis.
     ofstream outfile;
     // Menunjuk ke sebuah nama file
+ outfile.open(NamaFile = ".txt", ios::out);
+
+    cout << ">= Menulis file, \'q\' untuk keluar " << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "- ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' kedalam file
+        outfile << baris << endl;
+    }
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+
+    //membuka file dalam mode membaca
+    ifstream infile;
+
+    //menunjuk ke sebuah file
+    infile.open(NamaFile = ".txt", ios::in);
+
 };
--- iofile.cpp
+++ iofile.cpp
@@ -24,5 +24,31 @@ int main() {
     }    
  // selesai dalam menulis sekarang tutup filenya 
         outfile.close();
+         // membuka file dalam mode membaca
+        ifstream infile;
+        //menunjukan kesebuah file
+        infile.open("contohfile.txt");
+
+        cout << endl << ">= Membuka dan membaca file " << endl;
+        // jika file ada maka
+        if (infile.is_open())
+        {
+
+            // melakukan perulangan setiap baris
+            while (getline(infile,baris))
+            {
+                // dan tampilkan disini
+                cout << baris <<'\n';
+            }
+            // tutup file tersebut setelah selesai
+            infile.close();
+        }
+        // jika tidak ditemukan file maka akan menampilkan ini
+        else cout << "unable to open file" ;
+        return 0;
 
 };
+
+
+        
+
