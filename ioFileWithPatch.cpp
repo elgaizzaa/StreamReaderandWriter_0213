d07e006e18949dc6c3210b73b7c8b6df00e80ab0
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-06-04 15:09:16 +0700

    menambahkan variabel local namafile

---
+++ ioFileWithPatch.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string baris;
+    string NamaFile;
+
+    cout << "Masukan Nama File : ";
+    cin >> NamaFile;
+
+    // Membuka file dalam mode menulis.
+    ofstream outfile;
+    // Menunjuk ke sebuah nama file
+};
