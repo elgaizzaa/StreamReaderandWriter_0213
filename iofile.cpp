fe23c562b6b437a32a230af5ea55581e337fa8ad
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-06-04 14:54:04 +0700

    menambahkan carakter di line one

--- iofile.cpp
+++ iofile.cpp
@@ -15,4 +15,7 @@ int main() {
 //unlimeted loop untuk menulis
     while (true) {
         cout << "- ";
+     // mendapatkan setiap karakter dalam satu baris 
+        getline(cin, baris);
     }
+        
