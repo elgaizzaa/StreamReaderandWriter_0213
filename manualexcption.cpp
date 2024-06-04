b6af4443896ece8535b4ac51b31f3ec2de00c863
elgaizzaa <161436093+elgaizzaa@users.noreply.github.com>
2024-06-04 15:20:06 +0700

    menambahkan fungsi main

--- manualexcption.cpp
+++ manualexcption.cpp
@@ -1,2 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int main()
+{
+    try {
+        cout << "Selamat Belajar di prodid TI UMY" << endl;
+        //throw 0.5; // melempar sebuah integer maka
+        cout << "Pernyataan tidak akan diekesekusi " << endl;
+    }
+    catch (int a) {
+        //blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
+    }
+    catch(...) {
+        /*jika selain integer maka block ini akan dieksekusi*/
+        cout << "default Penegecualian diexsekusi" << endl;
+    }
+    return 0;
+}
